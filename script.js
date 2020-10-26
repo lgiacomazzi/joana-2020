a5bce51490c650537b9d9b8398395e07bb8b96c1
Luciano Giacomazzi <lucianogiacomazzi@Lucianos-MacBook-Pro.local>
2020-10-25 21:04:20 -0300

    Falta scroll efect

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 function leftScroll(arrow) {
   var portfolio = document.getElementById(arrow.parentElement.id);
   portfolio.scrollLeft -= portfolio.clientWidth;
+  if (portfolio.dataset.position > 0) {
+    portfolio.dataset.position = Number(portfolio.dataset.position) - 1;
+    console.log(Number(portfolio.dataset.position));
+  }
 }
 
 function rightScroll(arrow) {
   var portfolio = document.getElementById(arrow.parentElement.id);
   portfolio.scrollLeft += portfolio.clientWidth;
+  console.log(portfolio.width);
+  portfolio.dataset.position = Number(portfolio.dataset.position) + 1;
+  console.log(Number(portfolio.dataset.position));
 }
+
+// initiate parallax effects
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene);
+// you can change parallax options using parallaxInstance
